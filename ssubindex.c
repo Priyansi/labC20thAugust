e4229ff1aa5cee3b546d96dc13cfec8b71dd1481
Priyansi <13217677+Priyansi@users.noreply.github.com>
2019-10-28 13:56:20 +0530

    Added new program

---
+++ ssubindex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ char a[50];int u,i,l;
+ printf("Enter a string : ");
+ scanf("%[^\n]s",a);
+ printf("Enter the starting and ending index : ");
+ scanf("%i %i",&l,&u);
+ printf("The substring is : ");
+ for(i=l;i<u;i++)
+  printf("%c",a[i]);
+}
